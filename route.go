356bf21772a31b920b584209bb270cbb47fe9226
YuichiNAGAO <azkgfa03nagao@gmail.com>
2023-05-14 12:07:46 +0900

    AddPostハンドラの追加

--- route.go
+++ route.go
@@ -30,3 +30,18 @@ func getPosts(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(result)
 }
+
+func addPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var post Post
+	err := json.NewDecoder(r.Body).Decode(&post)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"message": "Error unmarshalling the request"}`))
+	}
+	post.Id = len(posts) + 1
+	posts = append(posts, post)
+	w.WriteHeader(http.StatusOK)
+	result, err := json.Marshal(posts)
+	w.Write(result)
+}
--- server.go
+++ server.go
@@ -15,6 +15,8 @@ func main() {
 		fmt.Fprintf(w, "Hello World")
 	}).Methods("GET")
 	route.HandleFunc("/posts", getPosts).Methods("GET")
+	route.HandleFunc("/posts", addPost).Methods("POST")
+	// curl -v -X POST -H "Content-Type: application/json" -d '{"id":1, "title":"タイトル", "text":"本文"}'  localhost:8080/posts
 
 	log.Println("Server running on port 8080")
 	log.Fatalln(http.ListenAndServe(":8080", route))
