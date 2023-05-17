68e2c7511a427e66c2d610b52a6e9ffd64abdb18
YuichiNAGAO <azkgfa03nagao@gmail.com>
2023-05-17 11:53:28 +0900

    package routerの導入

---
+++ router/mux_router.go
@@ -0,0 +1,31 @@
+package router
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type muxRouter struct{}
+
+func NewMuxRouter() Router {
+	return &muxRouter{}
+}
+
+var (
+	muxDispatcher = mux.NewRouter()
+)
+
+func (*muxRouter) GET(uri string, f func(w http.ResponseWriter, r *http.Request)) {
+	muxDispatcher.HandleFunc(uri, f).Methods("GET")
+}
+
+func (*muxRouter) POST(uri string, f func(w http.ResponseWriter, r *http.Request)) {
+	muxDispatcher.HandleFunc(uri, f).Methods("POST")
+}
+
+func (*muxRouter) SERVE(port string) {
+	log.Printf("Server running on port %v", port)
+	http.ListenAndServe(port, muxDispatcher)
+}
--- server.go
+++ server.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-
 	"github.com/YuichiNAGAO/go_clean_architecture_benkyo/controller"
 	"github.com/YuichiNAGAO/go_clean_architecture_benkyo/infrastructure"
 	"github.com/YuichiNAGAO/go_clean_architecture_benkyo/initializer"
-
-	gorilla "github.com/gorilla/mux"
+	"github.com/YuichiNAGAO/go_clean_architecture_benkyo/router"
 )
 
 func init() {
@@ -18,16 +13,16 @@ func init() {
 	infrastructure.ConnectDB()
 }
 
-func main() {
-	route := gorilla.NewRouter()
+var (
+	httpRouter = router.NewMuxRouter()
+)
 
-	route.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World")
-	}).Methods("GET")
-	route.HandleFunc("/posts", controller.GetPosts).Methods("GET")
-	route.HandleFunc("/posts", controller.AddPost).Methods("POST")
+func main() {
+	const port string = ":8080"
+
+	httpRouter.GET("/posts", controller.GetPosts)
+	httpRouter.POST("/posts", controller.AddPost)
 	// curl -v -X POST -H "Content-Type: application/json" -d '{"id":1, "title":"タイトル", "text":"本文"}'  localhost:8080/posts
 
-	log.Println("Server running on port 8080")
-	log.Fatalln(http.ListenAndServe(":8080", route))
+	httpRouter.SERVE(port)
 }
