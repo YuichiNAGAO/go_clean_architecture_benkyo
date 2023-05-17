8f26fa616374b0fcc7fbd2f02b2db5445e01f18a
YuichiNAGAO <azkgfa03nagao@gmail.com>
2023-05-17 13:24:35 +0900

    レポジトリのファイル名が間違っていたので修正

--- repository/mysql_repository.go
+++ repository/mysql_repository.go
@@ -1,8 +1,26 @@
 package repository
 
-import "github.com/YuichiNAGAO/go_clean_architecture_benkyo/entity"
+import (
+	"github.com/YuichiNAGAO/go_clean_architecture_benkyo/entity"
+	"github.com/YuichiNAGAO/go_clean_architecture_benkyo/infrastructure"
+)
 
-type PostRepository interface {
-	Save(post entity.Post) ([]entity.Post, error)
-	FindAll() ([]entity.Post, error)
+type repo struct{}
+
+func NewPostRepository() PostRepository {
+	return &repo{}
+}
+
+func (repo *repo) Save(post entity.Post) ([]entity.Post, error) {
+	var posts []entity.Post
+	infrastructure.DB.Find(&posts)
+	infrastructure.DB.Create(&post)
+	posts = append(posts, post)
+	return posts, nil
+}
+
+func (repo *repo) FindAll() ([]entity.Post, error) {
+	var posts []entity.Post
+	infrastructure.DB.Find(&posts)
+	return posts, nil
 }
